58da7c46a4c7296e76108200e324c8f2fe79db64
CreeperPlanet26 <fortniteplanet26>
2020-08-03 19:33:37 -0400

    better english

--- src/interfaces/CoolBotListConfig.ts
+++ src/interfaces/CoolBotListConfig.ts
@@ -1,23 +1,23 @@
 import { Client } from "discord.js";
 
 export default interface MainConfig {
-    /**
-     *  The api token.
-     */
-    token: string;
-    /**
-     *  The discord client used to make your bot.
-     */
-    client: Client;
-    /**
-     *  The interval in ms you want to send data to the api at.
-     * The default is 90000.
-     */
-    interval: number;
-    /**
-     *  If true it will send return data back to the methods.
-     *  If false it will not send return data back.
-     *  The default for this true
-     */
-    logging: boolean;
+  /**
+   *  The API token.
+   */
+  token: string;
+  /**
+   *  The discord client used to make your bot.
+   */
+  client: Client;
+  /**
+   *  The interval in ms you want to send data to the API at.
+   * The default is 90000.
+   */
+  interval: number;
+  /**
+   *  If true it will send return data back to the methods.
+   *  If false it will not send return data back.
+   *  The default for this true
+   */
+  logging: boolean;
 }
